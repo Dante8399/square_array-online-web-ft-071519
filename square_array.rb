941010e6c6691106e20cf89178d686aabecf7b1d
Dante Spencer <spencerdantel@gmail.com>
2019-07-20 20:43:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
- new_array = array.each do |square|
-   array(square) * array (square)
+ new_array = []
+ array.each do |square|
+   array[square] * array[square]
+   new_array << array
  end
- new_array
+ return new_array
 end
